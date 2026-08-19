c745fd897cfccf0c89194f83e76b1197b459b8f3
Hamen21 <hamenbhandari@gmail.com>
2026-08-19 13:42:13 +0530

    Complete SkillSwap frontend pages

--- skillswap-frontend/src/app/app.routes.ts
+++ skillswap-frontend/src/app/app.routes.ts
@@ -1,3 +1,19 @@
 import { Routes } from '@angular/router';
+import { Home } from './home/home';
+import { Register } from './register/register';
+import { Login } from './login/login';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    component: Home
+  },
+  {
+    path: 'register',
+    component: Register
+  },
+  {
+    path: 'login',
+    component: Login
+  }
+];
