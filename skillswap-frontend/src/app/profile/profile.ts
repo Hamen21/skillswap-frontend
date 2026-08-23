de659fb9337db92140f3729e322cd8b23bf5882e
Hamen21 <hamenbhandari@gmail.com>
2026-08-23 12:38:46 +0530

    Build interactive user profile

--- skillswap-frontend/src/app/profile/profile.ts
+++ skillswap-frontend/src/app/profile/profile.ts
@@ -1,9 +1,49 @@
 import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-profile',
-  imports: [],
+  imports: [RouterLink, CommonModule, FormsModule],
   templateUrl: './profile.html',
   styleUrl: './profile.css',
 })
-export class Profile {}
+export class Profile {
+
+  editing = false;
+
+name = 'Hamen Bhandari';
+
+bio = 'Computer Science student interested in learning and sharing technical skills.';
+education = 'B.Tech in Computer Science';
+teachSkills = ['Java', 'Python', 'HTML'];
+learnSkills = ['Angular', 'UI/UX'];
+newTeachSkill = '';
+newLearnSkill = '';
+
+ 
+
+
+  toggleEdit() {
+    this.editing = !this.editing;
+  }
+  addTeachSkill() {
+  const skill = this.newTeachSkill.trim();
+
+  if (skill && !this.teachSkills.includes(skill)) {
+    this.teachSkills.push(skill);
+    this.newTeachSkill = '';
+  }
+}
+
+addLearnSkill() {
+  const skill = this.newLearnSkill.trim();
+
+  if (skill && !this.learnSkills.includes(skill)) {
+    this.learnSkills.push(skill);
+    this.newLearnSkill = '';
+  }
+}
+
+}
