bb4d2a3a865265fa98e933b32f40fc1369dfb9ee
Hamen21 <hamenbhandari@gmail.com>
2026-08-23 11:09:35 +0530

    Improve skills page functionality

--- skillswap-frontend/src/app/skills/skills.ts
+++ skillswap-frontend/src/app/skills/skills.ts
@@ -1,10 +1,45 @@
 import { Component } from '@angular/core';
 import { RouterLink } from '@angular/router';
+import { FormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-skills',
-  imports: [RouterLink],
+  imports: [RouterLink, FormsModule, CommonModule],
   templateUrl: './skills.html',
   styleUrl: './skills.css',
 })
-export class Skills {}
+export class Skills {
+
+  teachSkills: string[] = [];
+  learnSkills: string[] = [];
+
+  teachInput = '';
+  learnInput = '';
+
+  addTeachSkill() {
+    const skill = this.teachInput.trim();
+
+    if (skill && !this.teachSkills.includes(skill)) {
+      this.teachSkills.push(skill);
+      this.teachInput = '';
+    }
+  }
+
+  addLearnSkill() {
+    const skill = this.learnInput.trim();
+
+    if (skill && !this.learnSkills.includes(skill)) {
+      this.learnSkills.push(skill);
+      this.learnInput = '';
+    }
+  }
+
+  removeTeachSkill(index: number) {
+    this.teachSkills.splice(index, 1);
+  }
+
+  removeLearnSkill(index: number) {
+    this.learnSkills.splice(index, 1);
+  }
+}
